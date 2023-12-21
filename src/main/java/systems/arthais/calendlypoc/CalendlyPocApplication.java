28358e295253856854588c5f993f9f4fc2199ce8
Raimundo do Espírito Santo <espirito.santo.arthais@outlook.com>
2023-12-21 20:55:52 -0300

    Carga inicial

---
+++ src/main/java/systems/arthais/calendlypoc/CalendlyPocApplication.java
@@ -0,0 +1,13 @@
+package systems.arthais.calendlypoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendlyPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendlyPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/systems/arthais/calendlypoc/CalendlyPocApplicationTests.java
@@ -0,0 +1,13 @@
+package systems.arthais.calendlypoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendlyPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
